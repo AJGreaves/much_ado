48efd10bbd2b03425d665d0bb8fe5035a8cfdaf5
A Greaves <anna.j.greaves@gmail.com>
2022-10-15 09:50:33 +0000

    Remove static commands.

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -179,8 +179,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
